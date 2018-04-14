9c6df649d60768238e3966b0ed2ac0d062c8a70b
Icia <icia83@gmail.com>
2018-04-14 19:58:54 +0200

    paint tasks

--- js/main.js
+++ js/main.js
@@ -1,18 +1,30 @@
 'use strict';
+//const declaration
+
 
 //Add header date
-function headerDate() {
+const headerDate = () => {
   const ACTUAL_DATE = new Date();
+  const WEEK_DAYS = ['Domingo','Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+  const MONTHS = ['Enero','Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+
   //get actual number day
-  let actualDay = ACTUAL_DATE.getDate();
-  document.querySelector('.actual__day-number').innerHTML = actualDay;
+  document.querySelector('.actual__day-number').innerHTML = ACTUAL_DATE.getDate();
   //get day of the week
-  let weekDays = ['Domingo','Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
-  document.querySelector('.actual__day').innerHTML = weekDays[ACTUAL_DATE.getDay()];
+  document.querySelector('.actual__day').innerHTML = WEEK_DAYS[ACTUAL_DATE.getDay()];
   //get month and year
-  let months = ['Enero','Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
-  let month = months[ACTUAL_DATE.getMonth()];
-  let year = ACTUAL_DATE.getFullYear();
-  document.querySelector('.actual__month-year').innerHTML = `${month}, ${year}`;
-}
+  document.querySelector('.actual__month-year').innerHTML = `${MONTHS[ACTUAL_DATE.getMonth()]}, ${ACTUAL_DATE.getFullYear()}`;
+};
 document.addEventListener('DOMContentLoaded', headerDate);
+
+
+//paint tasks
+function paintTasks() {
+  let taskList = document.querySelector('.task_list');
+  let newTask = document.getElementById('new_task').value;
+
+  taskList.innerHTML += `<li> ${newTask} </li>`;
+}
+
+let addNewTask = document.querySelector('.btn__add');
+addNewTask.addEventListener('click', paintTasks);
